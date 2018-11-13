e807aa7920fdf4753cff8f8a81f905e6312064b6
velezjose <velezq.jose@gmail.com>
2018-11-13 15:43:39 -0800

    Rotate banana dancer when clicked

--- src/bananaDancer.js
+++ src/bananaDancer.js
@@ -1,9 +1,18 @@
 var BananaDancer = function (top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  // this.$node.addClass('bananaDancer');
   this.$node.append('<img src="assets/pbj-guy.gif"/>');
-  
+  // Rotate banana dancer when clicked
+  this.$node.children().click((function () {
+    if (this.$node.children()[0].classList.contains('rotate-counter-clockwise')) {
+      this.$node.children().removeClass('rotate-counter-clockwise');
+      this.$node.children().addClass('rotate-clockwise');
+    } else {
+      this.$node.children().removeClass('rotate-clockwise');
+      this.$node.children().addClass('rotate-counter-clockwise');
+    }
+  }).bind(this));
 };
 
 BananaDancer.prototype = Object.create(Dancer.prototype);
 BananaDancer.prototype.constructor = BananaDancer;
+
